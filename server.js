f1f34bf631334b6f884b119f97ca3330195e382f
dadolfin1208 <bhensley1968@gmail.com>
2025-08-13 19:03:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
 // Root route
-app.get('/', (req, res) => 
-  res.send('FlowClient backend is live!'));
+app.get('/', (req, res) => {
+  res.send('FlowClient backend is running!');
+});
 
-
+// Other routes
 app.use('/clients', require('./routes/clients'));
 app.use('/invoices', require('./routes/invoices'));
 app.use('/ai', require('./routes/ai'));
